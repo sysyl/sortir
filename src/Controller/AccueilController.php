0a057ccb881d6013365944c9eb6ddfacc3cfad5c
Alhan <alhan.hilaire2019@campus-eni.fr>
2020-02-11 12:22:30 -0300

    Controller Accueil,Navbar,profil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -3,17 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/accueil", name="accueil")
+     * @param Request $request
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
      */
-    public function index()
+    public function index(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('index.html.twig', [
-            'controller_name' => 'AccueilController',
+        if ($this->getUser()) {
+            return $this->redirectToRoute('liste_sorties');
+        }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
         ]);
     }
 }
