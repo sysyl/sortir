badb7103b0f598ef96f667ab8b2ee053e574f32a
Maurice <metallicaman@hotmail.fr>
2020-02-11 17:02:06 +0100

    Login form in home page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,28 +19,28 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
 
-    /**
-     * @Route("/login", name="login")
-     * @param Request $request
-     * @param AuthenticationUtils $authenticationUtils
-     * @return Response
-     */
-    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
-    {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('liste_sorties');
-         }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error
-        ]);
-    }
+//    /**
+//     * @Route("/login", name="login")
+//     * @param Request $request
+//     * @param AuthenticationUtils $authenticationUtils
+//     * @return Response
+//     */
+//    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
+//    {
+//         if ($this->getUser()) {
+//             return $this->redirectToRoute('liste_sorties');
+//         }
+//
+//        // get the login error if there is one
+//        $error = $authenticationUtils->getLastAuthenticationError();
+//        // last username entered by the user
+//        $lastUsername = $authenticationUtils->getLastUsername();
+//
+//        return $this->render('security/login.html.twig', [
+//            'last_username' => $lastUsername,
+//            'error' => $error
+//        ]);
+//    }
 
     /**
      * @Route("/logout", name="logout")
